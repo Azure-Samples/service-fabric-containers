c50f28a16a9216abf13d0c72a22521a522fb068a
xtophs <christoph@bracketangles.net>
2017-10-10 15:50:34 +0200

    Added error detection and output to install.sh

--- Linux/container-tutorial/Voting/install.sh
+++ Linux/container-tutorial/Voting/install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+set -e
 
-sfctl application upload --path Voting --show-progress
-sfctl application provision --application-type-build-path Voting
-sfctl application create --app-name fabric:/Voting --app-type VotingType --app-version 1.0.0
+export filePath=Voting
+export appName=Voting
 
+echo Uploading Application Files
+sfctl application upload --path ${filePath} --show-progress
+
+echo Provisioning Application Type 
+sfctl application provision --application-type-build-path ${filePath}
+
+echo Creating Application
+sfctl application create --app-name fabric:/${appName} --app-type ${appName}Type --app-version 1.0.0
